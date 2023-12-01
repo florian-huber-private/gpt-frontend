b9c60adc70852405dba6cb3c164abadd3caf2f25
Florian Huber <146333739+florian-huber-private@users.noreply.github.com>
2023-12-01 14:35:06 +0100

    Implementierung der Kategorienverwaltung und Integration in das Frontend
    - Erstellung des CategoryService mit Funktionen zum Abrufen, Erstellen, Bearbeiten und Löschen von Kategorien.
    - Entwicklung der CategoryList Komponente zur Anzeige aller Kategorien mit Links zum Bearbeiten jeder Kategorie.
    - Entwicklung der CategoryForm Komponente für das Erstellen und Bearbeiten von Kategorien.
    - Integration des CategoryService in die CategoryList und CategoryForm Komponenten.
    - Anwendung von Bootstrap-Styling in den Kategorienverwaltungskomponenten für eine konsistente Benutzeroberfläche.

---
+++ src/components/categories/CategoryList.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../../services/CategoryService';
+import { ICategory } from '../../types/interfaces';
+
+const CategoryList: React.FC = () => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const fetchedCategories = await getCategories();
+        setCategories(fetchedCategories);
+      } catch (error) {
+        console.error('Fehler beim Laden der Kategorien:', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2 className="mb-4">Kategorienliste</h2>
+      <Link to="/create-category" className="btn btn-primary mb-3">Neue Kategorie erstellen</Link>
+      <ul className="list-group">
+        {categories.map((category) => (
+          <li key={category.id} className="list-group-item">
+            {category.name}
+            <Link to={`/edit-category/${category.id}`} className="btn btn-sm btn-secondary float-end">Bearbeiten</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/services/CategoryService.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:5000/categories';
+
+export const getCategories = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const getCategoryDetails = async (categoryId: number) => {
+  const response = await axios.get(`${API_URL}/${categoryId}`);
+  return response.data;
+};
+
+export const createCategory = async (categoryData: any) => {
+  const response = await axios.post(API_URL, categoryData);
+  return response.data;
+};
+
+export const updateCategory = async (categoryId: number, categoryData: any) => {
+  const response = await axios.put(`${API_URL}/${categoryId}`, categoryData);
+  return response.data;
+};
+
+export const deleteCategory = async (categoryId: number) => {
+  await axios.delete(`${API_URL}/${categoryId}`);
+};
